12e33bd7553e89b57d9215459468f87f602b0457
shinyay <shinya.com@gmail.com>
2018-08-11 22:34:06 +0900

    Initial Commit

---
+++ src/main/kotlin/io/pivotal/shinyay/api/ApiApplication.kt
@@ -0,0 +1,11 @@
+package io.pivotal.shinyay.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiApplication>(*args)
+}
