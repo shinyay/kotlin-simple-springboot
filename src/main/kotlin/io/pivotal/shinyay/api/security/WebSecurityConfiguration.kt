a91dee15e180e874c0d4573b8a3e7a1a12e3cffe
shinyay <shinya.com@gmail.com>
2018-08-24 16:06:37 +0900

    Add security configuration and remove the classes that are not in use

--- src/main/kotlin/io/pivotal/shinyay/api/security/WebSecurityConfiguration.kt
+++ src/main/kotlin/io/pivotal/shinyay/api/security/WebSecurityConfiguration.kt
@@ -4,6 +4,7 @@ import io.pivotal.shinyay.api.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
 import org.springframework.security.access.AccessDecisionManager
 import org.springframework.security.access.vote.AuthenticatedVoter
 import org.springframework.security.access.vote.RoleVoter
@@ -22,70 +23,19 @@ import java.util.*
 
 @Configuration
 @EnableWebSecurity
+@Order(1)
 class WebSecurityConfiguration : WebSecurityConfigurerAdapter() {
 
-    @Autowired
-    lateinit var service: UserService
-
-    /**
-     * Will be resolved into: WebSecurityEntryPoint injected instance.
-     */
-    @Autowired
-    lateinit var unauthorizedHandler: AuthenticationEntryPoint
-
-    @Autowired
-    lateinit var successHandler: WebSecurityAuthSuccessHandler
-
-    @Autowired
-    override fun configure(auth: AuthenticationManagerBuilder) {
-        auth.authenticationProvider(authenticationProvider())
-    }
-
-    override fun configure(http: HttpSecurity?) {
+    override fun configure(http: HttpSecurity) {
         http
-                ?.csrf()?.disable()
-                ?.exceptionHandling()
-                ?.authenticationEntryPoint(unauthorizedHandler)
-                ?.and()
-                ?.authorizeRequests()
-                /**
-                 * Access to Notes and Todos API calls is given to any authenticated system user.
-                 */
-                ?.antMatchers("/notes")?.authenticated()
-                ?.antMatchers("/notes/**")?.authenticated()
-                ?.antMatchers("/todos")?.authenticated()
-                ?.antMatchers("/todos/**")?.authenticated()
-                /**
-                 * Access to User API calls is given only to Admin user.
-                 */
-                ?.antMatchers("/users")?.hasAnyAuthority("ADMIN")
-                ?.antMatchers("/users/**")?.hasAnyAuthority("ADMIN")
-                ?.and()
-                ?.formLogin()
-                ?.successHandler(successHandler)
-                ?.failureHandler(SimpleUrlAuthenticationFailureHandler())
-                ?.and()
-                ?.logout()
-    }
-
-    @Bean
-    fun authenticationProvider(): DaoAuthenticationProvider {
-        val authProvider = DaoAuthenticationProvider()
-        authProvider.setUserDetailsService(service)
-        authProvider.setPasswordEncoder(encoder())
-        return authProvider
-    }
-
-    @Bean
-    fun encoder(): PasswordEncoder = BCryptPasswordEncoder(11)
-
-    @Bean
-    fun accessDecisionManager(): AccessDecisionManager {
-        val decisionVoters = Arrays.asList(
-                WebExpressionVoter(),
-                RoleVoter(),
-                AuthenticatedVoter()
-        )
-        return UnanimousBased(decisionVoters)
+                .httpBasic().disable().authorizeRequests()
+                .antMatchers("/notes").permitAll()
+                .antMatchers("/notes/**").permitAll()
+                .antMatchers("/todos").permitAll()
+                .antMatchers("/todos/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf().disable()
     }
 }
