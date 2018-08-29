9bed9f4c9141f8befcac0d931be92dfbf8788a5e
shinyay <shinya.com@gmail.com>
2018-08-29 21:24:52 +0900

    Remove import declaration is not needed anymore

--- src/main/kotlin/io/pivotal/shinyay/api/security/WebSecurityConfiguration.kt
+++ src/main/kotlin/io/pivotal/shinyay/api/security/WebSecurityConfiguration.kt
@@ -1,25 +1,10 @@
 package io.pivotal.shinyay.api.security
 
-import io.pivotal.shinyay.api.service.UserService
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
-import org.springframework.security.access.AccessDecisionManager
-import org.springframework.security.access.vote.AuthenticatedVoter
-import org.springframework.security.access.vote.RoleVoter
-import org.springframework.security.access.vote.UnanimousBased
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.web.AuthenticationEntryPoint
-import org.springframework.security.web.access.expression.WebExpressionVoter
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler
-import java.util.*
 
 @Configuration
 @EnableWebSecurity
