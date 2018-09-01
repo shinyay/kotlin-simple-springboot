06cfca53815dd25a9a35db0254f2daebf9f72d27
shinyay <shinya.com@gmail.com>
2018-09-01 18:13:57 +0900

    Add Spring Reactor EventBus module

--- src/main/kotlin/io/pivotal/shinyay/api/service/NoteService.kt
+++ src/main/kotlin/io/pivotal/shinyay/api/service/NoteService.kt
@@ -2,9 +2,12 @@ package io.pivotal.shinyay.api.service
 
 import io.pivotal.shinyay.api.data.Note
 import io.pivotal.shinyay.api.data.NoteDTO
+import io.pivotal.shinyay.api.reactor.NotesCountNotification
 import io.pivotal.shinyay.api.repository.NoteRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import reactor.bus.Event
+import reactor.bus.EventBus
 import java.util.*
 
 @Service("Note Service")
@@ -13,17 +16,28 @@ class NoteService {
     @Autowired
     private lateinit var repository: NoteRepository
 
+    @Autowired
+    private lateinit var eventBus: EventBus
+
     fun getNotes() : Iterable<NoteDTO> = repository.findAll().map { it -> NoteDTO(it) }
 
-    fun insertNote(note: NoteDTO): NoteDTO = NoteDTO(
-            repository.save(
-                    Note(
-                            title = note.title,
-                            message = note.message,
-                            location = note.location
-                    )
-            )
-    )
+    fun insertNote(note: NoteDTO): NoteDTO {
+        val result = NoteDTO(
+                repository.save(
+                        Note(
+                                title = note.title,
+                                message = note.message,
+                                location = note.location
+                        )
+                )
+        )
+        val count = getNotes().count()
+        if (count > 10) {
+            val notification = NotesCountNotification(count)
+            eventBus.notify("notesCountNotificationConsumer", Event.wrap(notification))
+        }
+        return result
+    }
 
     fun updateNote(noteDto: NoteDTO): NoteDTO {
         var note = repository.findOne(noteDto.id)
--- src/main/kotlin/io/pivotal/shinyay/api/service/TodoService.kt
+++ src/main/kotlin/io/pivotal/shinyay/api/service/TodoService.kt
@@ -2,9 +2,12 @@ package io.pivotal.shinyay.api.service
 
 import io.pivotal.shinyay.api.data.Todo
 import io.pivotal.shinyay.api.data.TodoDTO
+import io.pivotal.shinyay.api.reactor.TodosCountNotification
 import io.pivotal.shinyay.api.repository.TodoRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import reactor.bus.Event
+import reactor.bus.EventBus
 import java.util.*
 
 @Service("Todo Service")
@@ -13,18 +16,29 @@ class TodoService {
     @Autowired
     private lateinit var repository: TodoRepository
 
+    @Autowired
+    private lateinit var eventBus: EventBus
+
     fun getTodos(): Iterable<TodoDTO> = repository.findAll().map { it -> TodoDTO(it) }
 
-    fun insertTodo(todo: TodoDTO): TodoDTO = TodoDTO(
-            repository.save(
-                    Todo(
-                            title = todo.title,
-                            message = todo.message,
-                            location = todo.location,
-                            schedule = todo.schedule
-                    )
-            )
-    )
+    fun insertTodo(todo: TodoDTO): TodoDTO {
+        val result = TodoDTO(
+                repository.save(
+                        Todo(
+                                title = todo.title,
+                                message = todo.message,
+                                location = todo.location,
+                                schedule = todo.schedule
+                        )
+                )
+        )
+        val count = getTodos().count()
+        if (count > 10) {
+            val notification = TodosCountNotification(count)
+            eventBus.notify("todosCountNotificationConsumer", Event.wrap(notification))
+        }
+        return result
+    }
 
     fun updateTodo(todoDto: TodoDTO): TodoDTO {
         var todo = repository.findOne(todoDto.id)
